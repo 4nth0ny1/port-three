0167f8a9f0079e88702e21ba034eb75cf06a46a2
4nth0ny1 <acatullo4@gmail.com>
2021-11-17 19:52:49 -0600

    added navbar and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+
+  root to: "home#index"
+  # get 'about', to: 'about#index', as: 'about'
+  # get 'projects', to: 'projects#index', as: 'projects'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
