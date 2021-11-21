c4cef015f747206f04956bcdd45303ea836f6de6
4nth0ny1 <acatullo4@gmail.com>
2021-11-20 18:13:29 -0600

    removed jquery

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
 
   root to: "home#index"
-  # get 'about', to: 'about#index', as: 'about'
+  get 'about', to: 'about#index', as: 'about'
   # get 'projects', to: 'projects#index', as: 'projects'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
